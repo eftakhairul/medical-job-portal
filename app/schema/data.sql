bd6fdca691c92df0c12e6b1746efed850edd765a
Md. Eftakhairul Islam <eftakhairul@gmail.com>
2012-01-03 02:09:02 +0600

    Version 1.1 released

--- app/schema/data.sql
+++ app/schema/data.sql
@@ -1,21 +1,25 @@
 --  Eftakhairul Islam < eftakhairul@gmail.com> http://eftakhairul.com
--- 14 November, 2011
-
+-- 1 January, 2011
 
 SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
-INSERT INTO `user_types` (`user_type_id`, `title`) VALUES
-(1, 'Super Admin'),
-(2, 'Admin'),
-(3, 'Employer'),
-(4, 'Employee');
+INSERT INTO `user_types` (`types`) VALUES
+('admin'),
+('applicant'),
+('employer'),
+('super_admin');
 
-INSERT INTO `users` (`user_id`, `username`, `password`, `user_type_id`, `created_date`, `modified_date`) VALUES
-(1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', 2, '2011-11-14', '0000-00-00 00:00:00');
-
-
+INSERT INTO `users` (`user_id`, `username`, `password`, `types`, `created_date`, `modified_date`) VALUES
+(1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', 'admin', '2012-01-01', '0000-00-00 00:00:00');
 
 
+INSERT INTO `job_types` (`types`) VALUES
+('accounts'),
+('doctor'),
+('mpo'),
+('mr'),
+('nurse'),
+('parients care');
 
 SET FOREIGN_KEY_CHECKS=1;
--- app/schema/log.sql
+++ app/schema/log.sql
@@ -1,2 +1,96 @@
 --  Eftakhairul Islam < eftakhairul@gmail.com> http://eftakhairul.com
 -- 14 November, 2011
+
+SET FOREIGN_KEY_CHECKS=0;
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+CREATE TABLE IF NOT EXISTS `applicants` (
+  `user_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `address` text NOT NULL,
+  `contact_no` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `cv` varchar(1000) DEFAULT NULL,
+  `create_date` date NOT NULL,
+  `update_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `employers` (
+  `user_id` int(11) NOT NULL,
+  `doctor_name` varchar(100) NOT NULL,
+  `designation` varchar(50) NOT NULL,
+  `company_name` varchar(100) DEFAULT NULL,
+  `address` text,
+  `contact_number` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `website` varchar(100) DEFAULT NULL,
+  `create_date` date NOT NULL,
+  `update_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `jobs` (
+  `job_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `types` varchar(30) NOT NULL,
+  `title` varchar(150) NOT NULL,
+  `description` text NOT NULL,
+  `create_date` date NOT NULL,
+  `upated_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`job_id`),
+  KEY `user_id` (`user_id`),
+  KEY `types` (`types`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `job_boards` (
+  `job_board_id` int(11) NOT NULL AUTO_INCREMENT,
+  `job_id` int(11) NOT NULL,
+  `applicant_id` int(11) NOT NULL,
+  `create_date` date NOT NULL,
+  PRIMARY KEY (`job_board_id`),
+  KEY `job_id` (`job_id`,`applicant_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `job_types` (
+  `types` varchar(30) NOT NULL,
+  PRIMARY KEY (`types`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `password` varchar(35) CHARACTER SET utf8 NOT NULL,
+  `types` varchar(15) CHARACTER SET utf8 NOT NULL,
+  `created_date` date NOT NULL,
+  `modified_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username` (`username`),
+  KEY `types` (`types`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `user_types` (
+  `types` varchar(15) NOT NULL,
+  PRIMARY KEY (`types`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+ALTER TABLE `applicants`
+  ADD CONSTRAINT `applicants_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `employers`
+  ADD CONSTRAINT `employers_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `jobs`
+  ADD CONSTRAINT `jobs_ibfk_1` FOREIGN KEY (`types`) REFERENCES `job_types` (`types`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `jobs_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `job_boards`
+  ADD CONSTRAINT `job_boards_ibfk_1` FOREIGN KEY (`job_id`) REFERENCES `jobs` (`job_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`types`) REFERENCES `user_types` (`types`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+SET FOREIGN_KEY_CHECKS=1;
+
+
